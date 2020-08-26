ef710f8d397ee5136e8293c76c9b41bb7765210b
Rodney Smith II <r_smith3@hotmail.com>
2020-08-26 22:56:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,8 +43,7 @@ def monopoly_with_fourth_tier
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
-    },
+      },
     :names => {
       :reading_railroad => {mortgage_value: 100},
       :pennsylvania_railroad => {mortgage_value: 200},
@@ -52,4 +51,5 @@ def monopoly_with_fourth_tier
       :shortline => {mortgage_value: 800}
     }
    }
+}
 end
